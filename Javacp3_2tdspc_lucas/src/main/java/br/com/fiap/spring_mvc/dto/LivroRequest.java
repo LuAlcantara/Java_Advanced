f5ab9998258497cd644e1db43124b4b0107e8e1c
LuAlcantara <oalcantara.lucas@gmail.com>
2025-02-21 09:49:55 -0300

    Atualização_aula_210225_v2

--- Javacp3_2tdspc_lucas/src/main/java/br/com/fiap/spring_mvc/dto/LivroRequest.java
+++ Javacp3_2tdspc_lucas/src/main/java/br/com/fiap/spring_mvc/dto/LivroRequest.java
@@ -9,18 +9,17 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
-public record LivroRequest (
+@Data
+public class LivroRequest {
     @NotBlank(message = "O título é obrigatório")
-    String titulo,
+    private String titulo;
     @NotBlank(message = "O autor é obrigatório")
     @Size(min = 4, max = 150, message = "O nome do autor deve ter entre 4 e 150 caracteres")
-    String autor,
+    private String autor;
     @NotNull(message = "A categoria é obrigatória")
-    Categoria categoria,
+    private Categoria categoria;
     @DecimalMin(value = "0.99", message = "O preço deve ser maior que R$ 0.99")
-    BigDecimal preco,
+    private BigDecimal preco;
     @Pattern(regexp = "^970\\d{7}|^970\\d{10}$", message = "O ISBN deve seguir o padrão internacional")
-    String isbn
-) {
-
+    private String isbn;
 }
